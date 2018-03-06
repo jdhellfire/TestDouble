8babc947479f206a24da61dc361a7189adb4ddc7
jdhellfire <jdhellfire@126.com>
2018-03-06 22:15:49 +0800

    RF: interface_get_read_to_go run pass

--- CarControllerUT.py
+++ CarControllerUT.py
@@ -5,15 +5,18 @@ from GearBox import GearBox
 from Electronics import Electronics
 from StatusPanel import StatusPanel
 
+
 class ControllerUT(unittest.TestCase):
+    def setUp(self):
+        self.ctrl = CarController()
+
     def test_interface_get_ready_to_go(self):
         """
         GIVEN :engine, gearbox, electronics, status_panel
         WHEN  :call get_ready_to_go interface
         THEN  :return true
         """
-        ctrl = CarController()
-        self.assertTrue(ctrl.get_ready_to_go(Engine(),GearBox(),Electronics(),StatusPanel()))
+        self.assertTrue(self.ctrl.get_ready_to_go(Engine(), GearBox(), Electronics(), StatusPanel()))
 
 
 if __name__ == '__main__':
