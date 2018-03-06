11bf4b87edd775f6864894233a05e7f86c241a8d
jdhellfire <jdhellfire@126.com>
2018-03-06 22:09:41 +0800

    RED:add get_read_to_go UT and run fail

--- CarControllerUT.py
+++ CarControllerUT.py
@@ -1,8 +1,16 @@
 import unittest
+from CarController import *
 
 
 class ControllerUT(unittest.TestCase):
-    pass
+    def test_interface_get_ready_to_go(self):
+        """
+        GIVEN :engine, gearbox, electronics, status_panel
+        WHEN  :call get_ready_to_go interface
+        THEN  :return true
+        """
+        ctrl = CarController()
+        self.assertTrue(ctrl.get_ready_to_go(Engine(),GearBox(),Electronics(),StatusPanel()))
 
 
 if __name__ == '__main__':
