202c9f8468c6954983fba794ace84ec990cf0089
jdhellfire <jdhellfire@126.com>
2018-03-06 23:01:32 +0800

    RF go_forward ut 001,002 合并，抽取测试数据

--- CarControllerUT.py
+++ CarControllerUT.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest import mock
+from TestData import *
 from CarController import CarController
 from Engine import Engine
 from GearBox import GearBox
@@ -29,32 +30,14 @@ class ControllerUT(unittest.TestCase):
         """
         panel = StatusPanel()
         electronics = Electronics()
-
         electronics.accelerate = mock.Mock(return_value=None)
-        panel.there_is_enough_fuel = mock.Mock(return_value=True)
-        electronics.engine_is_running = mock.Mock(return_value=True)
+
+        for Data in TestData['GO_FORWARD']:
+            panel.there_is_enough_fuel = mock.Mock(return_value=Data['FUEL_IS_ENOUGH'])
+            electronics.engine_is_running = mock.Mock(return_value=Data['ENGINE_IS_RUNNING'])
 
         self.ctrl.go_forward(electronics, panel)
-        self.assertEqual(1,electronics.accelerate.call_count)
-
-    def test_interface_go_forward_002(self):
-        """
-        GIVEN :engine, status_panel,CarController
-        GIVEN :CarController call go_forward interface
-        WHEN  :electronics.engine_is_running() return flase
-        WHEN  :status_panel.there_is_enough_fuel() return True
-        THEN  :electronics.accelerate will not be call
-        """
-        panel = StatusPanel()
-        electronics = Electronics()
-
-        electronics.accelerate = mock.Mock(return_value=None)
-        panel.there_is_enough_fuel = mock.Mock(return_value=True)
-        electronics.engine_is_running = mock.Mock(return_value=False)
-
-        self.ctrl.go_forward(electronics, panel)
-        self.assertEqual(0, electronics.accelerate.call_count)
-
+        self.assertEqual(Data['CALLED_CNT'],electronics.accelerate.call_count)
 
 if __name__ == '__main__':
     unittest.main()
---
+++ TestData.py
@@ -0,0 +1,11 @@
+TestData = \
+    {
+
+        'GO_FORWARD': (
+                        {'FUEL_IS_ENOUGH':True,'ENGINE_IS_RUNNING':True,'CALLED_CNT': 1},
+                        {'FUEL_IS_ENOUGH': False, 'ENGINE_IS_RUNNING': True, 'CALLED_CNT': 0},
+                        {'FUEL_IS_ENOUGH': True, 'ENGINE_IS_RUNNING': False, 'CALLED_CNT': 0},
+                        {'FUEL_IS_ENOUGH': False, 'ENGINE_IS_RUNNING': False, 'CALLED_CNT': 0},
+                    )
+
+    }
